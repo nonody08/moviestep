9e79c589e68361c683eba3f875159acd670f5895
Wesley <wesleylundu32@gmail.com>
2026-05-11 23:10:26 +0100

    changé le placeholder du titre du film

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -18,7 +18,7 @@ function FilmForm({ onAdd }) {
     <form className="task-form" onSubmit={handleSubmit}>
       <input
         type="text"
-        placeholder="Titre du film..."
+        placeholder="Entrez le film..."
         value={titre}
         onChange={(e) => setTitre(e.target.value)}
       />
